aabd06de17e3c07fd1c03c7887ba783bee420e3f
Ami Reynoso <amireynoso@gmail.com>
2015-06-09 17:18:30 -0300

    Mobile nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -62,6 +62,38 @@ var currentNum = 0;
 		$(this).alterClass('bg-*', getNewClass());
 	});
 
+	$('body').on('change', '.menu-trigger input', function(){
+		if( $(this).prop('checked') ) {
+			$('body').addClass('nav-open');
+		} else {
+			$('body').removeClass('nav-open');
+		}
+	});
+
+	$('img.svg').each(function(){
+		var $img = $(this);
+		var imgID = $img.attr('id');
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+
+		$.get(imgURL, function(data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = $(data).find('svg');
+			// Add replaced image's ID to the new SVG
+			if (typeof imgID !== 'undefined') {
+				$svg = $svg.attr('id', imgID);
+			}
+			// Add replaced image's classes to the new SVG
+			if (typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass+' replaced-svg');
+			}
+			// Remove any invalid XML tags as per http://validator.w3.org
+			$svg = $svg.removeAttr('xmlns:a');
+			// Replace image with new SVG
+			$img.replaceWith($svg);
+		});
+	});
+
 })(jQuery);
 
 /***************************************************
