db9818918fbc25547a32990e2d5992122b0900d4
Ami Reynoso <amireynoso@gmail.com>
2017-04-07 21:26:01 -0300

    Updated dependencies and information

--- gulpfile.js
+++ gulpfile.js
@@ -123,3 +123,8 @@ gulp.task('build', ['html', 'images', 'fonts', 'extras'], function () {
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
 });
+
+gulp.task('deploy', function() {
+  return gulp.src('dist/**/*')
+    .pipe($.ghPages());
+});
