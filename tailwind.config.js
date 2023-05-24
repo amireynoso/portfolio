ae7cb2d70010458a808445d10d38390578a7f14f
Ami Reynoso <amireynoso@users.noreply.github.com>
2023-05-23 18:26:34 -0700

    Configure Tailwind some more

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ["Fira Code", ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        shopify: "#95bf47",
+        meli: "#fff159",
+        latcom: "#103078",
+        datafactory: "#ae1a1f",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
